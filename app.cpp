76a2db5597c6d8fa33aff1ed27c3c8b073ed9f6d
Simon <swu.sim@gmail.com>
2017-02-19 23:47:54 -0800

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -2,14 +2,8 @@
 #include "view.h"
 
 int main() {
-	
-	// Create an instance/object of the Controller class
 	Controller controller;
-
-	// Create an instance/object of the View class
-	// (controller) to show where controller is located so it can communicate with it
 	View view(controller);
-
 	// Display the Main Menu
 	view.displayMainMenu();
 }

7bbfffa1417b2cff2bf1d9d2ad912e918c53eec6
Simon <swu.sim@gmail.com>
2017-02-19 23:44:07 -0800

    Update controller.h

--- controller.h
+++ controller.h
@@ -9,14 +9,12 @@ using namespace std;
 class Controller
 {
 	private:
-		// Declare a class variable type StringParser and the compiler will create a StringParser object automatically
 		StringParser	stringparser;
 
 	public:
 		Controller();
 		~Controller();
 
-		// & = Address of _______
 		void letterCounts(string text, int counts[]);
 		int wordCount(string text);
 		void wordList(string text, vector<string> &words);
